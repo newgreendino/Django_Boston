5841389be3854d8958f8283639dbe26039d78518
newgreendino <tichymartin@email.cz>
2017-07-25 10:56:23 +0200

    upgrade settings

--- Main_folder/settings.py
+++ Main_folder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e1)z*#48gtx6b$8t4@scxvhu^9*z%*by3mz%05+i-2epb46cxy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'greendino.pythonanywhere.com']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
